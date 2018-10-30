2b01d015052881d0b73cfb112db4fb7cf2689b0a
Nouhoum Traoré <nouhoum.traore@gmail.com>
2018-10-30 23:05:23 +0100

    Add examples for integers

---
+++ types/numbers.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("1 + 1 =", 1 + 1)
+  fmt.Println("3 * 4 =", 3 * 4)
+  fmt.Println("120 - 4 =", 120 - 4)
+  fmt.Println("120 / 2 =", 120 / 2)
+  fmt.Println("120 % 2 =", 120 % 2)
+  fmt.Println("5 % 2 =", 5 % 2)
+}
