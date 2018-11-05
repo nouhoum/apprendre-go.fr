4bf9d368ef6f8a2fddd804c845793518a02e83bd
Nouhoum Traoré <nouhoum.traore@gmail.com>
2018-11-06 00:59:37 +0100

    Declare variables

---
+++ variables/basics.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  var (
+    likes int
+    name string
+    ok bool
+  )
+
+  fmt.Println(likes)
+  fmt.Println(name)
+  fmt.Println(ok)
+
+  var city, country string
+
+  fmt.Println(city)
+  fmt.Println(country)
+}
